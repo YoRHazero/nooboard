02bf813227011d6d5e398733f32c40edfc7ba96a
zchao <zchao@icrr.u-tokyo.ac.jp>
2026-02-28 05:17:01 +0900

    refactor(app): remove legacy app usecase modules

--- crates/nooboard-app/src/service/app/subscriptions.rs
+++
@@ -1,9 +0,0 @@
-use crate::AppResult;
-
-use super::{AppServiceImpl, EventSubscription};
-
-impl AppServiceImpl {
-    pub(super) async fn subscribe_events_usecase(&self) -> AppResult<EventSubscription> {
-        self.subscriptions.subscribe().await
-    }
-}
